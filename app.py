a7b42663c60c09033f1ac575edf8534c4f4cc357
s1f102500236 <s1f102500236@iniad.org>
2025-07-02 00:57:00 +0900

    add code
    コードを追加した

--- app.py
+++ app.py
@@ -23,6 +23,10 @@ def answer():
         
     kinoko_percent=kinoko_count / (kinoko_count + takenoko_count) *100
     takenoko_percent=takenoko_count / (kinoko_count + takenoko_count) *100
+    message_html=''
+    for i in range(len(message)):
+        message=message[i]
+        message_html +='<div class="alert {1}" role="alert">{0}</div>\n'.format(message, 'alert-warning ms-5' if i % 2==0 else 'alert-sucess me-5')
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
